d31be114fe3500b0f5c73057fcf095a6ee42b4c5
Szleaves <steven-zh@foxmail.com>
2023-04-17 14:09:03 +0800

    update: conv-input batch bigru x6

--- run/2.model_bigru.py
+++ run/2.model_bigru.py
@@ -159,7 +159,7 @@ def model_bigru(
     :return:               (bigru_model, ctc_model) 返回构建的BiGRU模型和CTC Loss模型
     """
     # 双向GRU单位层数
-    GRU_NUMS = 2
+    GRU_NUMS = 3
 
     # 定义模型输入数据格式 (输入格式与ctc_batch_generator的返回值一致)
     input_data = Input(name="X", shape=(None, n_mfcc))
@@ -278,7 +278,7 @@ end = time.time() - start
 print("-- Times: %.2fs --" % end)
 
 # 保存模型
-bigru_model.save(FILES_PATH + "models/test/conv/bigru-conv-x4.h5")
+bigru_model.save(FILES_PATH + "models/test/conv/bigru-conv-x6.h5")
 # 保存训练数据
-with open(FILES_PATH + "models/test/conv/bigru-conv-x4-h.pkl", "wb") as file:
+with open(FILES_PATH + "models/test/conv/bigru-conv-x6-h.pkl", "wb") as file:
     pickle.dump(history.history, file)
