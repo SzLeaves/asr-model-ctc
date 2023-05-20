42b1ede6a9436bb695c38828a8c9250b34aefbf8
Szleaves <steven-zh@foxmail.com>
2023-05-20 10:41:51 +0800

    remove: old files

--- run/train_3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# nohup jupyter nbconvert --to notebook --execute --allow-errors --inplace ../3.model_wavenet.ipynb > run_3.log 2>&1 &
-nohup python3 3.model_wavenet.py > run_3.log 2>&1 &
